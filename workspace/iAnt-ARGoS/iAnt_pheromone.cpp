db0dd1405590b7462e19663289e0964d611e4a61
Antonio-Griego <deano505x@gmail.com>
2014-09-12 18:02:24 -0600

    major updates and retooling to detach iAnt_data_structures

---
+++ workspace/iAnt-ARGoS/iAnt_pheromone.cpp
@@ -0,0 +1,77 @@
+#include "iAnt_pheromone.h"
+
+iAnt_pheromone::iAnt_pheromone() :
+	lastUpdated(0),
+	decayRate(0.0),
+	weight(0.0),
+	threshold(0.0),
+	isActive(false)
+{}
+
+iAnt_pheromone::iAnt_pheromone(const iAnt_pheromone &copy) {
+	location = copy.location;
+	lastUpdated = copy.lastUpdated;
+	decayRate = copy.decayRate;
+	weight = copy.weight;
+	threshold = copy.threshold;
+	isActive = copy.isActive;
+}
+
+iAnt_pheromone::iAnt_pheromone(CVector2 loc, long int tick, double decay, double w) {
+	location = loc;
+	decayRate = decay;
+	weight = w;
+	threshold = w * 0.001;
+	lastUpdated = tick;
+	isActive = true;
+}
+
+iAnt_pheromone::~iAnt_pheromone() {
+	// unused
+}
+
+// Returns decay of quantity at time given rate of change lambda
+void iAnt_pheromone::Update(long int time) {
+    weight = weight * exp(-decayRate * (time - lastUpdated));
+    lastUpdated = time;
+
+    if(weight <= threshold) isActive = false;
+}
+
+// reset the pheromone
+void iAnt_pheromone::Reset(CVector2 loc, long int tick, double w) {
+	location = loc;
+	weight = w;
+	threshold = w * 0.001;
+	lastUpdated = tick;
+	isActive = true;
+}
+
+// change the decay rate
+void iAnt_pheromone::SetDecay(double decay) {
+	decayRate = decay;
+}
+
+// is the pheromone valid?
+bool iAnt_pheromone::IsActive() {
+	return isActive;
+}
+
+// pheromone strength or weight
+double iAnt_pheromone::Strength() {
+	return weight;
+}
+
+// pheromone location
+CVector2 iAnt_pheromone::Location() {
+	return location;
+}
+
+void iAnt_pheromone::Set(iAnt_pheromone newPheromone) {
+	location = newPheromone.location;
+	lastUpdated = newPheromone.lastUpdated;
+	decayRate = newPheromone.decayRate;
+	weight = newPheromone.weight;
+	threshold = newPheromone.threshold;
+	isActive = newPheromone.isActive;
+}
---
+++ workspace/iAnt-ARGoS/iAnt_pheromone.h
@@ -0,0 +1,38 @@
+#ifndef IANT_PHEROMONE_H_
+#define IANT_PHEROMONE_H_
+
+#include <argos3/core/utility/math/vector2.h>
+
+using namespace argos;
+
+class iAnt_pheromone {
+
+public:
+
+	iAnt_pheromone();
+	iAnt_pheromone(const iAnt_pheromone &copy);
+	iAnt_pheromone(CVector2 loc, long int tick, double decay, double w = 1.0);
+	~iAnt_pheromone();
+
+	void Update(long int time); // decay at time given with rate of change lambda
+	void Reset(CVector2 loc, long int tick, double w = 1.0); // reset the pheromone
+	void SetDecay(double decay); // change the decay rate
+	bool IsActive(); // is the pheromone valid and active?
+	double Strength(); // pheromone strength or weight
+	CVector2 Location(); // pheromone location
+	void Set(iAnt_pheromone newPheromone); // set pheromone = newPheromone
+
+private:
+
+	CVector2 location;
+
+	long int lastUpdated;
+
+	double decayRate;
+	double weight;
+	double threshold;
+
+	bool isActive;
+};
+
+#endif /* IANT_PHEROMONE_H_ */
